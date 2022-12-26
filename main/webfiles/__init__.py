dbe056dba933aa069995cd1e2ad701c8c6f5416a
Samyar <samyar1380@gmail.com>
2022-12-27 01:02:27 +0330

    Views blueprint, index route
    - Created and registered a blueprint for public views
    - Created route and view function for index page

--- main/webfiles/__init__.py
+++ main/webfiles/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .views import views
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "qwerty"
+
+    app.register_blueprint(views, url_prefix='')
+
     return app
