61ad108355ae9c49d6353a8edfab8a5b69553801
Samyar <samyar1380@gmail.com>
2022-12-27 01:11:19 +0330

    Authentication views blueprint and routes
    - Created and registered a blueprint for views related to authentication and privacy
    - Created routes and functions for log in, log out and sign up

--- main/webfiles/__init__.py
+++ main/webfiles/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from .views import views
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "qwerty"
 
     app.register_blueprint(views, url_prefix='')
+    app.register_blueprint(auth, url_prefix='/auth/')
 
     return app
