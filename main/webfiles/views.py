977d0f866a803a155697d4d42d677728a19a8d72
Samyar <samyar1380@gmail.com>
2023-01-09 19:40:33 +0330

    Homepage
    - Added a form to add new notes, and a table of all the notes the user had created above it
    - Added a delete button for a every note

--- main/webfiles/views.py
+++ main/webfiles/views.py
@@ -1,9 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, flash, redirect
 from flask_login import login_required, current_user
+from . import models, db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    return render_template("home.html", user=current_user)
+    if request.method == 'GET':
+        return render_template('home.html', user=current_user)
+    else:
+        data = request.form
+        if len(data['note']) >= 1:
+            new_note = models.Note(
+                text = data['note'],
+                user_id = current_user.id
+            )
+            db.session.add(new_note)
+            db.session.commit()
+            flash("Note was created successfully.", category='Success')
+        else:
+            flash("You need to type something.", category='Error')
+
+        return render_template('home.html', user=current_user)
+
+
+@views.route('/delete/', methods=['POST',])
+@login_required
+def delete():
+    if request.method == 'POST':
+        note_id = request.form.get('note_id')
+        note = models.Note.query.filter_by(id=note_id, user_id=current_user.id).first()
+
+        db.session.delete(note)
+        db.session.commit()
+        flash("Note was deleted successfully", category='Success')
+        return redirect(url_for('views.index'))
